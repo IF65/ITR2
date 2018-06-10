a06265a4908599d18bd5953fbbc9167e9f8ffcd3
Marco Gnecchi <marco.gnecchi@icloud.com>
2018-06-10 10:18:45 +0200

    Initial Commit

---
+++ ITR2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ITR2
+//
+//  Created by if65 on 10/06/2018.
+//  Copyright © 2018 if65. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
